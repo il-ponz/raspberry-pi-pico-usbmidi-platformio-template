e7df790b341f6955fd977498c2ef27995117cbb5
il ponz <ponz1ss@gmail.com>
2024-05-24 07:55:22 +0200

    setup midi monitor tested with bitwig

--- src/main.cpp
+++ src/main.cpp
@@ -10,48 +10,76 @@
 Adafruit_USBD_MIDI usbMidi;
 MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usbMidi, MIDI);
 
+// -----------------------------------------------------------------------------
+
+// This example shows the old way of checking for input messages.
+// It's simpler to use the callbacks now, check out the dedicated example.
+
+#define LED 13 // LED pin on Arduino Uno
+
+// -----------------------------------------------------------------------------
+
+
+
 // Example of handling midi input to the device
-void handle_midi_note_on(byte channel, byte note, byte velocity) {
-  Serial.println("Got note on!");
+// void handle_midi_note_on(byte channel, byte note, byte velocity) {
+//   Serial.printf("Got note on! {0} {1} {2}", channel, note, velocity );
+// }
+
+
+void BlinkLed(byte num) // Basic blink function
+{
+  // for (byte i = 0; i < num; i++)
+  // {
+    digitalWrite(LED, HIGH);
+    delay(50);
+    digitalWrite(LED, LOW);
+    delay(50);
+  // }
+  Serial.println(num);
 }
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Hello World!");
 
-  TinyUSBDevice.setManufacturerDescriptor("MadsKjeldgaard");
-  TinyUSBDevice.setProductDescriptor("Pico Blinkity Blinky");
+// -----------------------------------------------------------------------------
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("start midi sample");
+
+  // Set the MIDI note on handling function
+  // MIDI.setHandleNoteOn(handle_midi_note_on);
+
+  pinMode(LED, OUTPUT);
+
+  TinyUSBDevice.setManufacturerDescriptor("il ponz");
+  TinyUSBDevice.setProductDescriptor("mongos_midi_destroyer");
 
   usbMidi.begin();
-  MIDI.begin();
+  // Launch MIDI, by default listening to channel 1.
+  MIDI.begin(MIDI_CHANNEL_OMNI);
 
   // Turn midi thru off
   MIDI.turnThruOff();
-
-  // Set the MIDI note on handling function
-  MIDI.setHandleNoteOn(handle_midi_note_on);
-
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, 1);
 }
 
-auto pastLEDState = true;
-void loop() {
-  MIDI.read();
-  
-  Serial.println("Hello from da loop!");
-
-  // Blink the LED
-  digitalWrite(LED_BUILTIN, static_cast<int>(pastLEDState));
-
-  // Send a midi note
-  if(pastLEDState){
-    MIDI.sendNoteOn(60, 127, 1);
-  } else {
-    MIDI.sendNoteOff(60, 0, 1);
+void loop()
+{
+  if (MIDI.read()) // Is there a MIDI message incoming ?
+  {
+    switch (MIDI.getType()) // Get the type of the message we caught
+    {
+    case midi::ProgramChange:    // If it is a Program Change,
+      BlinkLed(MIDI.getData1()); // blink the LED a number of times
+                                 // correponding to the program number
+                                 // (0 to 127, it can last a while..)
+      break;
+    // See the online reference for other message types
+    case midi::NoteOn:
+      BlinkLed(MIDI.getData1());
+      break;
+    default:
+      break;
+    }
   }
-
-  pastLEDState = !pastLEDState;
-
-  delay(500);
 }
